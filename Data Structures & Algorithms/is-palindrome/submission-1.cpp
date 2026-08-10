7f4bc4118b978633eafe3810b8c0d36649dd8e00
Bùi Xuân Nhất <nhatbui2004.work@gmail.com>
2026-08-10 18:48:46 +0700

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+
+
+    bool isPalindrome(string s) {
+        int l = 0 , r = s.size()-1;
+
+        while(l < r){
+            while(!isalnum(s[l])) l++;
+            while(!isalnum(s[r])) r--;
+
+            if(l < r && tolower(s[l]) != tolower(s[r])) return false;
+
+            l++;
+            r--;
+        }
+
+        return true;
+    }
+};
