267cb4294dae90bdfdcd0efd8391c2956be954c8
Bùi Xuân Nhất <nhatbui2004.work@gmail.com>
2026-08-11 15:50:52 +0700

    Add: trapping-rain-water - submission-0

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-0.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int right = height.size()-1;
+        int left = 0 ;
+
+        int total = 0; 
+        int leftMax = height[0];
+        int rightMax = height[right];
+
+        while(left < right){
+            if(height[left] < height[right]){
+
+                leftMax = max(leftMax,height[left]);
+                if(leftMax - height[left] > 0){
+                    total = total + leftMax - height[left];
+                };
+
+                left++;
+            }
+            else{
+                
+                rightMax = max(rightMax,height[right]);
+                if(rightMax - height[right] > 0){
+                    total = total + rightMax - height[right];
+                }
+
+                right--; 
+            }
+        }
+
+        return total;
+    }
+};
